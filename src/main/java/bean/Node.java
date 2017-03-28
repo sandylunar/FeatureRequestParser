e35a53fdebf39cc209755672e6ce6851c7d241da
zzyo1995 <zzyo@hust.edu.cn>
2017-03-28 22:26:16 +0800

    tag 加粗

--- src/main/java/bean/Node.java
+++ src/main/java/bean/Node.java
@@ -46,14 +46,14 @@ public class Node {
             result = "\t"+result;
 
         return result;*/
-        String result = "<li><span class=\"file\">"+"["+tag+"]<br>"+sentence+"\n"+"</span>";
+        String result = "<li><span class=\"file\"><strong>"+tag+"<br></strong>"+sentence+"\n"+"</span>";
         if (hasChildren()){
             result += "<ul>";
             for(Node node : children){
                 String tmp = node.toString();
                 //System.out.println(result);
                 if (node.parent.sentence.equals("")){
-                    tmp = tmp.replaceFirst("\\[.+?\\]<br>", "");
+                    tmp = tmp.replaceFirst("(?<=<strong>).+?(?=<\\/strong>)", "");
                 }
                 result += tmp;
             }
